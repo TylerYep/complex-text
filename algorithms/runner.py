6ee1d375f32d60372a9cd52b5bbfc594deaf0ff6
harry <harryshahai@gmail.com>
2018-11-18 21:24:06 -0800

    start experiements add remove dup (buggy) clean up get results

--- algorithms/runner.py
+++ algorithms/runner.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 import util
+import itertools
+from tqdm import tqdm
 
 import os, sys
 sys.path.append('preprocess')
@@ -30,21 +32,25 @@ AdaBoost
 wb_path = 'preprocess/weebit_features.pkl'
 
 def get_results(alg: Algorithm, data, feature_lists, options_c, options_wc, options_tfidf):
-    for f in feature_lists:
-        for c in options_c:
-            for wc in options_wc:
-                for t in options_tfidf:
-                    if 'tfidf' not in f: t = {}
-                    if 'word count' not in f: wc = {}
-                    alg.run(data, f, c, wc, t)
+    prod = itertools.product(feature_lists, options_c, options_wc, options_tfidf)
+    for f, c, wc, t in tqdm(list(prod)):
+        if not('tfidf' in f or t == {}): continue # Ignore the case where theres not tfidf in features but tfdif is not {}
+        if not('word count' in f or wc == {}): continue
+        alg.run(data, f, c, wc, t)
+
+wc_opts = [{}, {'min_df':5}, {'max_df':0.8}, {'min_df':5, 'max_df':0.8}, {'min_df':5, 'max_df':0.8, 'binary':True}]
+tfidf_opts = [{}, {'min_df':5}, {'max_df':0.8}, {'min_df':5, 'max_df':0.8}]
+lr_opts = [{'penalty':'l1', 'C':1}, {'penalty':'l1', 'C':0.8}, {'penalty':'l1', 'C':0.6}, {'penalty':'l2', 'C':1}, {'penalty':'l2', 'C':0.8}, {'penalty':'l2', 'C':0.6}]
+
+features = [[x] for x in util.features] + [['word count', 'nl'], ['tfidf', 'nl'], ['word count', 'tfidf'], ['word count', 'tfidf', 'nl']]
 
 
+#names = ["Nearest_Neighbors", "SVM", "Gaussian_Process",
+#         "Decision_Tree", "Random_Forest", "Neural_Net", "AdaBoost",
+#         "Naive_Bayes", "Logistic_Regression", 'Dummy']
 if __name__ == "__main__":
-    a = algs.load_alg('Naive_Bayes')
-    features = util.features
+    a = algs.load_alg('Logistic_Regression')
     data = util.load_pkl(wb_path)
-    get_results(a, data, features, nb_opts, wc_opts, tfidf_opts)
-    a.run(data, ['tfidf'],wc_params={'min_df':5}, tfidf_params={'min_df':5} )
-    #a.run(data, ['word count'], wc_params={'min_df':4})
-    #a.run(data, ['tfidf'], tfidf_params={'min_df':5})
+    get_results(a, data, features, lr_opts, wc_opts, tfidf_opts)
+    #get_results(a, data, [['nl']], [{}], [{}], [{}]) Dummy: features ..etc don't matter
     a.to_csv()
